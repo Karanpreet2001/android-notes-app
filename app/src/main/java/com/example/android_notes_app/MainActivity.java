813a9b1e33cc3c088006346138c117886a963088
Karanpreet <karanpreetfarwaha@gmail.com>
2022-08-22 15:15:07 -0700

    adding dependencies

--- app/src/main/java/com/example/android_notes_app/MainActivity.java
+++ app/src/main/java/com/example/android_notes_app/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.main), "This a bar", Snackbar.LENGTH_SHORT);
+        snackbar.show();
+
+
     }
 }
